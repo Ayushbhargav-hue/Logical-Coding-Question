f0848ab7bf0329664a2d282586a4e1059a683e6b
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-13 16:40:49 +0530

    Create uniqueelementsarray.java

---
+++ uniqueelementsarray.java
@@ -0,0 +1,16 @@
+
+public class Uniqueelementsinarray {
+
+	public static void main(String[] args) {
+		int[] arr={1,1,2,2,2,2,2,3,3,4};
+		int i=0;
+	for(int j=0;j<arr.length;j++) {
+		if(arr[i]!=arr[j]) {
+			arr[i+1]=arr[j];
+			i++;
+		}
+	}
+	System.out.println(i+1);
+	}
+
+}
