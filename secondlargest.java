f4ed60b8ec4a452f4aa28ffada10df7604c39670
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-13 16:01:09 +0530

    Add files via upload

---
+++ secondlargest.java
@@ -0,0 +1,16 @@
+
+public class secondlargest {
+
+	public static void main(String[] args) {
+		int[] arr={5,5,5,5,5,5};
+		int a=arr[0],sl=-1;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]>a) {
+				sl=a;
+				a=arr[i];
+			}
+		}
+		System.out.println(sl);
+	}
+
+}
