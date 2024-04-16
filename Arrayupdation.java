0ce986f828ff2e33d8163c3917dd6f42f180065b
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-16 23:07:10 +0530

    Create Arrayupdation.java

---
+++ Arrayupdation.java
@@ -0,0 +1,10 @@
+class Arrayupdation
+{
+	public static void main(String[] args) {
+  int[] arr={1, 2, 3, 4, 5};
+  int updateindex=2;
+  int updatevalue=26;
+  arr[updateindex]=updatevalue;
+  for(int j:arr)System.out.print(j+" ");
+	}
+}
