af0b8b485b20e7ae648bdcd3cf61875af853bb78
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-24 17:16:10 +0530

    Add files via upload

---
+++ oddlocationelementpresentinjava.java
@@ -0,0 +1,12 @@
+
+public class oddlocationelementpresentinjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char[] arr= {'A','d','j','k','a'};
+for(int i=0;i<arr.length;i++) {
+	if(i%2!=0)System.out.print(arr[i]+" ");
+}
+	}
+
+}
