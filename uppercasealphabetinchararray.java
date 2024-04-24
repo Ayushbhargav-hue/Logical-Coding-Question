7eb4b6bf8485215c4f7d5f5668caf867e2542097
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-24 16:40:45 +0530

    Add files via upload

---
+++ uppercasealphabetinchararray.java
@@ -0,0 +1,12 @@
+
+public class uppercasealphabetinchararray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char[] arr= {'A','a','Z','f'};
+for(char temp:arr) {
+	if(temp>='A'&&temp<='Z')System.out.println("Uppercase letter  "+temp);
+}
+	}
+
+}
