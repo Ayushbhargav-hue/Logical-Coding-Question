81ee19a68021cead805cec7acb05cab43a46f8b1
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-24 16:42:19 +0530

    Add files via upload

---
+++ lowercasealphabetinarray.java
@@ -0,0 +1,12 @@
+
+public class lowercasealphabetinarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] arr= {'A','a','Z','f'};
+		for(char temp:arr) 
+			if(temp>='a'&&temp<='z')System.out.println("Lowercase letter  "+temp);
+		
+	}
+
+}
